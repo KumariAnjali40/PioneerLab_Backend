ea526fc5ce8c91dc41bce86b1153bfa061914669
KumariAnjali40 <142905766+KumariAnjali40@users.noreply.github.com>
2024-03-26 15:50:28 +0530

    login register done

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const {connection}= require('./db');
+const {userRouter}= require("./routes/user.routes")
+
+const app = express();
+
+app.use(express.json());
+app.use('/user',userRouter)
+
+app.get('/',(req,res)=>{
+    res.send("hello")
+})
+
+app.listen(4500,async()=>{
+ try{
+    await connection,
+    console.log("connected to DB");
+    console.log("Server is runnig at the port 4500")
+ }catch(err){
+    console.log(err)
+ }
+})
